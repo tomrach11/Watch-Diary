66d6bec333a541f281b7129aac11a33d782a16cf
tomrach11 <tomrach11@gmail.com>
2021-01-08 11:53:49 -0500

    first commit

---
+++ watchdiary/src/main/java/com/team4/watchdiary/WatchdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.team4.watchdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchdiaryApplication.class, args);
+	}
+
+}
---
+++ watchdiary/src/test/java/com/team4/watchdiary/WatchdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.watchdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
