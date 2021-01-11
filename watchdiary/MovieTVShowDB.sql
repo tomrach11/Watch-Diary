bad5b66a7ecbb0cfa87579bd8e4433e85daf495b
cjthebossCS1122 <42753850+cjthebossCS1122@users.noreply.github.com>
2021-01-11 15:18:29 -0500

    Next draft

--- watchdiary/MovieTVShowDB.sql
+++ watchdiary/MovieTVShowDB.sql
@@ -4,30 +4,44 @@ USE MovieTVShowDB;
 
 CREATE TABLE Movie (
 	movie_Id INT PRIMARY KEY AUTO_INCREMENT,
-    name varchar(20),
-    watched BOOLEAN DEFAULT false,
-    towatch BOOLEAN DEFAULT false
+    Title varchar(20),
+    Note varchar(225),
+    Rating tinyint,
+    ViewDate date,
+    Watched BOOLEAN DEFAULT false,
+    ToWatch BOOLEAN DEFAULT false
     );
     
 CREATE TABLE TVShow (
 	tvshow_Id INT PRIMARY KEY AUTO_INCREMENT,
-    name varchar(20),
-    watched BOOLEAN DEFAULT false,
-    towatch BOOLEAN DEFAULT false
+    Title varchar(20),
+    Note varchar(225),
+    Rating tinyint,
+    ViewDate date,
+    Watched BOOLEAN DEFAULT false,
+    ToWatch BOOLEAN DEFAULT false
     ); 
 CREATE TABLE Season (
 	season_Id INT PRIMARY KEY AUTO_INCREMENT,
-    name varchar(20),
-    watched BOOLEAN DEFAULT false,
-    towatch BOOLEAN DEFAULT false,
+    Title varchar(20),
+    Note varchar(225),
+    Rating tinyint,
+    ViewDate date,
+    Watched BOOLEAN DEFAULT false,
+    ToWatch BOOLEAN DEFAULT false,
+    tvshow_Id int,
     foreign key fk_Season_TVShow (tvshow_Id)
 		references TVShow(tvshow_Id)
     );
 CREATE TABLE Episode (
 	episode_Id INT PRIMARY KEY AUTO_INCREMENT,
-    name varchar(4),
-    watched BOOLEAN DEFAULT false,
-    towatch BOOLEAN DEFAULT false,
+    Title varchar(20),
+    Note varchar(225),
+    Rating tinyint,
+    ViewDate date,
+    Watched BOOLEAN DEFAULT false,
+    ToWatch BOOLEAN DEFAULT false,
+    season_Id int,
     foreign key fk_Episode_Season (season_Id)
 		references Season(season_Id)
     );    
