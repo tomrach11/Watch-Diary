ec0a43f839e00f5c5fb60e80c9206ac856787c9f
Daniel Catlett <daniel.catlett42@gmail.com>
2021-01-14 16:52:41 -0600

    done for the night

--- MovieSearch_FrontEnd/js/index.js
+++ MovieSearch_FrontEnd/js/index.js
@@ -1,16 +1,36 @@
 $(document).ready(function ()
 {
-    addListItem();
-    addListItem();
-    addListItem();
-    addListItem();
-    addListItem();
+    displayMovies();
 });
 
-function addListItem()
+function addListItem(text)
 {
     var node = document.createElement("li");
-    var textNode = document.createTextNode("Movie 4");
+    var textNode = document.createTextNode(text);
     node.appendChild(textNode);
     document.getElementById("list").appendChild(node);
+}
+
+async function getAllMovies()
+{
+    let url = 'http://localhost:8080/api/movies';
+    try
+    {
+        let res = await fetch(url);
+        return await res.json();
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+async function displayMovies()
+{
+    window.alert("here");
+    let movies = await getAllMovies();
+
+    movies.forEach(movie => {
+        addListItem(movie.title);
+    })
 }
