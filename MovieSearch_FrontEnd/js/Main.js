0d9896592015dcf48d7b9e47ac64ee2ddafe137e
KadyPei <kp1940@nyu.edu>
2021-01-14 12:25:31 -0800

    Updates on Search Page
    added display of movie search results

--- MovieSearch_FrontEnd/js/Main.js
+++ MovieSearch_FrontEnd/js/Main.js
@@ -1,16 +1,36 @@
 $(document).ready(()=>{
     $('#searchForm').on('submit', (e)=>{
         //console.log($('#searchText').val());
+
         let searchText = $('#searchText').val();
         getMovies(searchText);
         e.preventDefault();
+
     });
 });
 
 function getMovies(searchText){
-    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=e9497f8'+searchText)
+   // var apikey ="e9497f8"
+    axios.get('http://www.omdbapi.com/?s='+searchText+'&apikey=e9497f8')
     .then((response)=>{
         console.log(response);
+        //put in an array of search movies
+        let movies = response.data.Search;
+        let output = '';
+        $.each(movies,(index,movie)=>{
+            output += `
+                <div class = "col-md-3">
+                    <div class="well text-center">
+                        <img src = "${movie.Poster}">
+                        <h5>${movie.Title}</h5>
+                        <a onclick="movieSelected('${movie.imdbID}')" class ="btn btn=primary" href ="#">Movie details</a>
+                    </div>
+                </div>
+            `;
+        });
+
+        $('#movies').html(output);
+
     })
     .catch((err)=>{
         console.log(err);
