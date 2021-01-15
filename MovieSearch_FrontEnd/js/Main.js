4e76ce13a16e6cacd657493638e192a56422faa4
KadyPei <kp1940@nyu.edu>
2021-01-15 09:01:31 -0800

    SearchPage, DetailPage, Main.js
    More changes to the search page and a working detailpage with movie api

--- MovieSearch_FrontEnd/js/Main.js
+++ MovieSearch_FrontEnd/js/Main.js
@@ -23,7 +23,7 @@ function getMovies(searchText){
                     <div class="well text-center">
                         <img src = "${movie.Poster}">
                         <h5>${movie.Title}</h5>
-                        <a onclick="movieSelected('${movie.imdbID}')" class ="btn btn=primary" href ="#">Movie details</a>
+                        <a onclick="movieSelected('${movie.imdbID}')" class ="btn btn-primary" href ="#">View Details</a>
                     </div>
                 </div>
             `;
@@ -35,4 +35,50 @@ function getMovies(searchText){
     .catch((err)=>{
         console.log(err);
     });
+}
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location='DetailPage.html';
+    return false;
+}
+function getMovie(){
+    let movieId = sessionStorage.getItem('movieId');
+     axios.get('http://www.omdbapi.com/?i='+movieId+'&apikey=e9497f8')
+        .then((response)=>{
+            console.log(response);
+            let movie = response.data;
+
+            let output = `
+                <div class = "row">
+                    <div class = "col-md-4">
+                        <img src = "${movie.Poster}" class = "thumbnail">
+                    </div>
+                    <div class = "col-md-8">
+                        <h2>${movie.Title}</h2>
+                        <ul class= "list-group">
+                            <li class = "list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                            <li class = "list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                            <li class = "list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                            <li class = "list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                            <li class = "list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                            <li class = "list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                            <li class = "list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class ="row">
+                    <div class ="well">
+                        <h3>Plot</h3>
+                        ${movie.Plot}
+                        <hr>
+                        <a href="http://www.imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">Go to Website</a>
+                        <a href="SearchPage.html" class="btn btn-default">Go back to Search</a>
+            `;
+            $('#movie').html(output);
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+
 }
