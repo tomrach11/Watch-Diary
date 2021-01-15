ef126157ce2249457db656f5596542b001eb89b8
Daniel Catlett <daniel.catlett42@gmail.com>
2021-01-15 09:59:14 -0600

    Frontend able to call backend api and get a valid response

--- MovieSearch_FrontEnd/js/index.js
+++ MovieSearch_FrontEnd/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function ()
 {
-    displayMovies();
+    getMovies();
 });
 
 function addListItem(text)
@@ -8,29 +8,17 @@ function addListItem(text)
     var node = document.createElement("li");
     var textNode = document.createTextNode(text);
     node.appendChild(textNode);
-    document.getElementById("list").appendChild(node);
+    document.getElementById("actualList").appendChild(node);
 }
 
-async function getAllMovies()
+function getMovies()
 {
-    let url = 'http://localhost:8080/api/movies';
-    try
-    {
-        let res = await fetch(url);
-        return await res.json();
-    }
-    catch(error)
-    {
-        console.log(error);
-    }
+    $.ajax({url: 'http://localhost:8080/api/movies'}).done(data => useMovies(data));
 }
 
-async function displayMovies()
+function useMovies(data)
 {
-    window.alert("here");
-    let movies = await getAllMovies();
-
-    movies.forEach(movie => {
+    data.forEach(movie => {
         addListItem(movie.title);
-    })
+    });
 }
